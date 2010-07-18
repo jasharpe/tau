ce3dd7e618b9d589d5d3fb04ab745617892a2f56
Ali Reza Alibhai <aralibhai@ara.(none)>
2010-07-18 15:21:27 -0700

    Adding Card and Deck classes

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+
+public final class Card {
+
+    public final int colour, number, fill, shape;
+    public Card(int colour, int number, int fill, int shape) {
+        this.colour = colour;
+        this.number = number;
+        this.fill = fill;
+        this.shape = shape;
+    }
+
+}
