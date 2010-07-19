f220dba969ba411702eafdb1d001f4119af4f5fa
Ali Reza Alibhai <aralibhai@ara.(none)>
2010-07-18 23:26:20 -0700

    times2mod3

--- src/com/taugame/tau/shared/Card.java
+++ src/com/taugame/tau/shared/Card.java
@@ -2,6 +2,7 @@ package com.taugame.tau.shared;
 
 public final class Card {
     private static final int COLOUR = 0, NUMBER = 1, FILL = 2, SHAPE = 3;
+    private static final int[] TIMES_TWO_MOD_THREE = new int[] {0, 2, 1, 0, 2};
     private final int[] properties;
 
     public Card(int colour, int number, int fill, int shape) {
@@ -13,22 +14,9 @@ public final class Card {
     }
 
     public static boolean isSet(Card card1, Card card2, Card card3) {
-        int value;
         for (int property = 0; property < 4; property++) {
-            if ((value = card1.get(property)) == card2.get(property)) {
-                if (value != card3.get(property)) return false;
-            }
-            else {
-                int sum = card1.get(property) + card2.get(property);
-                switch (sum) {
-                case 1: value = 2; break;
-                case 2: value = 1; break;
-                case 3: value = 0; break;
-                }
-                if (card3.get(property) != value) {
-                    return false;
-                }
-            }
+            int sum = card1.get(property) + card2.get(property);
+            if (card3.get(property) != TIMES_TWO_MOD_THREE[sum]) return false;
         }
         return true;
     }
